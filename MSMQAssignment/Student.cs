a080d4b5a65496b934a8026bd65a51a776053519
MJPS <MJPS@MOHAMMEDJOS8F9A>
2016-09-19 12:41:43 +0200

    Student Class

--- MSMQAssignment/Student.cs
+++ MSMQAssignment/Student.cs
@@ -11,6 +11,20 @@ namespace MSMQAssignment
 
         public string Name { get; set; }
 
+        public int Age { get; set; }
+
+        public List<String> Degrees { get; set; }
+
+
+        public Student(string name, int age, List<String> degrees)
+        {
+            this.Age = age;
+            this.Name = name;
+            degrees = new List<string>();
+        }
+
+
+        
 
     }
 }
